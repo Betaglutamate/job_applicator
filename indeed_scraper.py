1f63405907efd024057fe8129a6bc2903753b0eb
Mark Zurbruegg <39518170+Betaglutamate@users.noreply.github.com>
2022-04-11 15:36:08 +0100

    implemented scraper and text generator prompt

---
+++ indeed_scraper.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import time
+
+def get_job_data(job_keywords, location):
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"
+    }
+
+    job_keywords = ['data', 'engineer']
+    location =  'United+States'
+    joined_keywords = '+'.join(job_keywords)
+
+    url = f"https://www.indeed.com/jobs?q={joined_keywords}&l={location}"
+    api_url = "https://www.indeed.com/viewjob?viewtype=embedded&jk={job_id}"
+
+    soup = BeautifulSoup(requests.get(url, headers=headers).content, "html.parser")
+
+    job_listing = {}
+
+    for job in soup.select('a[id^="job_"]')[0:2]:
+        job_id = job["id"].split("_")[-1]
+        s = BeautifulSoup(
+            requests.get(api_url.format(job_id=job_id), headers=headers).content,
+            "html.parser",
+        )
+        
+        job_listing[job_id] = s.title.get_text(strip=True) + s.select_one("#jobDescriptionText").get_text(strip=True, separator="\n")
+
+    return job_listing
+        # print(s.title.get_text(strip=True))
+        # print()
+        # print(
+        #     s.select_one("#jobDescriptionText").get_text(strip=True, separator="\n")
+        # )
+        # print("#" * 80)
--- text_generator.py
+++ text_generator.py
@@ -2,47 +2,41 @@ import os
 import openai
 from decouple import config
 
+from indeed_scraper import get_job_data
+
+job_data = get_job_data(['data', 'engineer'], location='United+States')
+
 api_key = config('OPENAI_API_KEY')
 
-response = openai.Completion.create(
+
+for k,v in job_data.items():
+  v_clean = v.replace('\n', ' ')
+  response = openai.Completion.create(
   engine="text-davinci-002",
-  prompt="""
-  write a job application for the following job description
+  prompt=f"""
+  generate a letter applying to the following job
 
-job description: "Job Description
-
-General Summary:
-
-The Cell Biology and Protein Biochemistry Senior Research Scientist works with some independence and is adept at conceptually outlining studies to design and execute focused research projects or a series of complex experimental procedures. These may include mammalian cell culture manipulation and maintenance, generation and use of cell lines derived from ex vivo material and differentiated stem cells as well as performing cellular and protein-based assays. Completes work in a resourceful, self-sufﬁcient manner and is able to design alternative approaches to achieve desired outcomes. Keeps current on the scientiﬁc literature relevant to scientiﬁc methodology or ﬁeld(s) of technical expertise. This level captures both the Research organization’s entry level for staff with a research doctoral degree (PhD) and the advanced experimental expert leading one or more critical laboratory functions with high impact on drug discovery projects.
-
-Key Duties and Responsibilities:
-
-Designs and conducts elaborate, conceptually connected, multi-component experiments which use a variety of techniques acquired from cellular biology and protein sciences, which could include:
-Mammalian cell culture, manipulation, and maintenance
-Generation and use of cell lines derived from ex vivo material and differentiated stem cells
-Protein science techniques including mutagenesis and recombinant protein production
-Molecular biology techniques including genotyping, PCR, and cloning
-Gene expression approaches including qPCR, FACS, ICC, and Western blot
-In vitro phenotyping methods including ELISA and other assays
-Collates and interprets data systematically, and synthesizes results into a cohesive body of conclusions or recommendations to guide Project decisions and new research activities
-Exercises solid judgment to prioritize studies, considering feasibility and Project impact
-Performs advanced experimental troubleshooting
-Scours relevant scientiﬁc literature and routinely incorporates new insights into research activities
-Identiﬁes, prioritizes and introduces relevant emerging technologies in ﬁeld of expertise to advance the existing technology platforms and create/maintain a competitive advantage
-Explores the feasibility of applying new scientiﬁc principles/concepts or implements and validates new experimental approaches and technologies to achieve project goals
-Independently prepares study presentations and presents experimental conclusions at internal Group/Department or Project Team research meetings
-Performs other duties as assigned"
+  job description: {v_clean}
 
 
-job application:""",
+  application:""",
   
   temperature=0.3,
-  max_tokens=60,
+  max_tokens=1000,
   top_p=1,
   frequency_penalty=0.5,
   presence_penalty=0
 )
 print(response)
 
-response.keys()
-response['choices'][0]
+written_application = response.choices[0]['text']
+## add my name
+written_application.replace('[Your name]', 'Mark Zurbruegg')
+
+
+
+
+# response.keys()
+# response['choices'][0]
+
+
